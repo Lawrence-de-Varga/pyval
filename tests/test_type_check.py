8e46b841a0ef942c6aacaa8764af4360fe26f32b
Lawrence de Varga <lawrencedevarga@gmail.com>
2025-06-27 23:52:48 +1000

    adding tests for type_check_args

--- tests/test_type_check.py
+++ tests/test_type_check.py
@@ -9,15 +9,31 @@ from pyval.type_check import type_check_args, type_check_args_strict
 
 class TestTypeCheck(unittest.TestCase):
     def test_type_check_no_arg_types(self):
-        @type_check_args([])
-        def mul(x, y):
-            return x * x
+        with self.assertRaises(ValueError):
 
-        self.assertRaises(ValueError)
+            @type_check_args([])
+            def mul(x, y):
+                return x * x
 
     def test_type_check_args_not_type_list(self):
-        @type_check_args(["hello", 5, True])
-        def mul(x, y):
-            return x * y
+        with self.assertRaises(TypeError):
 
-        self.assertRaises(TypeError)
+            @type_check_args(["hello", 5, True])
+            def mul(x, y):
+                return x * y
+
+    def test_type_check_args_too_many_types(self):
+        with self.assertRaises(ValueError):
+
+            @type_check_args([str, int, float, str])
+            def mul(x, y):
+                return x * y
+
+            mul(7, 8)
+
+    def test_type_check_ars_not_list(self):
+        with self.assertRaises(TypeError):
+
+            @type_check_args(str, list)
+            def mul(x, y):
+                return x * y
